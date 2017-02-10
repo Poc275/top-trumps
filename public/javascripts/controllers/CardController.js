67ac745f34bfe2fbcf59ba526da69894497a1d34
Poc275 <poc275@gmail.com>
2017-02-10 23:49:08 +0000

    Displaying data from db

---
+++ public/javascripts/controllers/CardController.js
@@ -0,0 +1,12 @@
+angular.module('CardsModule').controller('CardController', function($scope, $http) {
+	$scope.cards = [];
+
+	$scope.getCards = function() {
+		$http.get('/cards').success(function(data) {
+			console.log(data);
+			$scope.cards = data;
+		});
+	};
+
+	$scope.getCards();
+});
