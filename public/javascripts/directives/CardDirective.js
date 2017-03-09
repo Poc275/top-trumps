5623a5b5710031126ad5f20d2da6d0af6837a210
Poc275 <poc275@gmail.com>
2017-03-09 21:39:29 +0000

    Card flipping to show biographies implemented
    Uses animate.css (https://daneden.github.io/animate.css/)

--- public/javascripts/directives/CardDirective.js
+++ public/javascripts/directives/CardDirective.js
@@ -3,6 +3,7 @@ angular.module('CardsModule').directive('card', function() {
 		restrict: 'E',
 		templateUrl: '/templates/card.html',
 		controller: function($scope, Cards) {
+
 			Cards.find('Donald Trump').success(function(data) {
 				$scope.card = data;
 			});
--- test/e2e-tests.js
+++ test/e2e-tests.js
@@ -5,3 +5,25 @@ describe('Protractor Demo Tests', function() {
     expect(browser.getTitle()).toEqual('Top C****!');
   });
 });
+
+
+describe('Card flip tests', function() {
+	beforeEach(function() {
+		browser.get('http://localhost:3000/#/auth/callback?user=poc275@gmail.com');
+	});
+
+	it('card should not be flipped initially', function() {
+		var title = element(by.css('.md-headline'));
+
+		expect(title.getText()).toEqual('Donald Trump');
+	});
+
+	it('card should flip when info button is clicked', function() {
+		var infoButton = element(by.css('.md-icon-button'));
+		infoButton.click();
+
+		var title = element(by.css('.md-headline'));
+
+		expect(title.getText()).toEqual('Biography');
+	});
+});
